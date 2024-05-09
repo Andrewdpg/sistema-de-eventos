ced0dc7fef6cc093f841bfff4feec3a11ab20ae3
Silem Nabib <silemnabib@gmail.com>
2024-05-08 22:27:53 -0500

    feat: add user login and logout functionality

--- src/users/urls.py
+++ src/users/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
 from users import views
+from pages import views as pages_views
 
 urlpatterns = [
-    path('', views.signup, name='signup'),
+    path('signup/', views.signup, name='signup'),
+    path('signin/', views.signin, name='signin'),
+    path('signout/', views.signout, name='signout'),
+    path('', pages_views.home, name='home'),
 ]
--- src/users/views.py
+++ src/users/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth import authenticate, login
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import authenticate, login, logout
 
 def signup(request):
     if request.user.is_authenticated:
@@ -23,3 +23,27 @@ def signup(request):
     else:
         form = UserCreationForm()
         return render(request, 'users/signup.html', {'form': form})
+    
+def signin(request):
+    if request.user.is_authenticated:
+        return redirect('home')
+    
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        else:
+            msg = 'Error Login'
+            form = AuthenticationForm(request.POST)
+            return render(request, 'users/login.html', {'form': form, 'msg': msg})
+    else:
+        form = AuthenticationForm()
+        return render(request, 'users/login.html', {'form': form})
+    
+def signout(request):
+    logout(request)
+    return redirect('home')
