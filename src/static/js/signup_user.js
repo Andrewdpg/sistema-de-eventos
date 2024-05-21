d37adebc116804f8f4533ee01d435ba6087ffb76
SilemNabib <silemnabib@gmail.com>
2024-05-21 18:12:54 -0500

    build: add js listeners for changes in register fields

---
+++ src/static/js/signup_user.js
@@ -0,0 +1,43 @@
+document.getElementById('paises').addEventListener('change', function() {
+    var paisId = this.value;
+    fetch('/get_departamentos/?pais_id=' + paisId)
+        .then(response => response.json())
+        .then(data => {
+            var select = document.getElementById('departamentos');
+            select.innerHTML = '';
+
+            var option = document.createElement('option');
+            option.value = 0;
+            option.text = "Departamento";
+            select.appendChild(option);
+            
+            for (var i = 0; i < data.length; i++) {
+                option = document.createElement('option');
+                option.value = data[i][0];  // codigo
+                option.text = data[i][1];  // nombre
+                select.appendChild(option);
+            }
+        });
+});
+
+document.getElementById('departamentos').addEventListener('change', function() {
+    var dptoId = this.value;
+    fetch('/get_ciudades/?dpto_id=' + dptoId)
+        .then(response => response.json())
+        .then(data => {
+            var select = document.getElementById('ciudades');
+            select.innerHTML = '';
+
+            var option = document.createElement('option');
+            option.value = 0;
+            option.text = "Ciudad";
+            select.appendChild(option);
+            
+            for (var i = 0; i < data.length; i++) {
+                option = document.createElement('option');
+                option.value = data[i][0];  // codigo
+                option.text = data[i][1];  // nombre
+                select.appendChild(option);
+            }
+        });
+});
