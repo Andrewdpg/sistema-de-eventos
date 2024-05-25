c52160d6a10d51806080c261005c77b1b841f1b2
SilemNabib <silemnabib@gmail.com>
2024-05-25 08:46:01 -0500

    build: add documents format for mongoDB

--- src/mongodb_documents.py
+++ src/mongodb_documents.py
@@ -1,4 +1,33 @@
-def evento(data):
+def employee_doc(nombre_usuario, password, identificacion):
+    employee = {
+        "last_login": None,
+        "identificacion": identificacion,
+        "nombre_usuario": nombre_usuario,
+        "password": password,
+        "is_superuser": False,
+    }
+
+    return employee
+
+def user_doc(identificacion, nombre_usuario, password, nombres, apellidos, tipo_empleado, email, pais, departamento, ciudad):
+    user = {
+        "last_login": None,
+        "identificacion": identificacion,
+        "nombre_usuario": nombre_usuario,
+        "password": password,
+        "is_superuser": False,
+        "nombres": nombres,
+        "apellidos": apellidos,
+        "tipo_empleado": tipo_empleado,
+        "email": email,
+        "pais": pais,
+        "departamento": departamento,
+        "ciudad": ciudad,
+    }
+
+    return user
+
+def evento_doc(titulo, descripcion, categorias, fecha, lugar_datos, conferencistas, facilitadores, facultades_org, programa_org, img_url=None):
     """
     Create a MongoDB document based on the input data.
     :param data_to_parse: dictionary with the input data
@@ -6,15 +35,33 @@ def evento(data):
     """
 
     event_document = {
-        "titulo": data['titulo'],
-        "img_url": data['img_url'],
-        "descripcion": data['descripcion'],
-        "categorias": data['categorias'],
-        "fecha": data['fecha'],
-        "lugar": data['lugar'],
-        "conferencistas": data['conferencistas'],
-        "facilitadores": data['facilitadores'],
-        "organizadores": data['organizadores'], 
+        "titulo": titulo,
+        "descripcion": descripcion,
+        "categorias": categorias,
+        "fecha": fecha,
+        "conferencistas": conferencistas,
+        "facilitadores": facilitadores,
+        "facultades_org": facultades_org,
     }
+
+    lugar = {
+        "nombre" : lugar_datos[0],
+        "direccion" : lugar_datos[1],
+    }
+
+    ciudad_datos = {
+        "nombre": lugar_datos[2][0],
+        "departamento": lugar_datos[2][1],
+        "pais": lugar_datos[2][2],
+    }    
+
+    lugar["ciudad"] = ciudad_datos
+    event_document["lugar"] = lugar
+
+    if programa_org is not None:
+            event_document["programa_org"] = programa_org  
+
+    if img_url is not None:
+        event_document["img_url"] = img_url
 
     return event_document
