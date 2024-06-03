492907cf3daa1a976c0c938c2a0597ff2acb996c
SilemNabib <silemnabib@gmail.com>
2024-06-02 23:53:58 -0500

    feat: add default user menu

--- src/event_log/urls.py
+++ src/event_log/urls.py
@@ -4,7 +4,7 @@ from django.views.generic.base import RedirectView
 
 urlpatterns = [
     path("", RedirectView.as_view(url='e/', permanent=False)),
-    path('e/', event_views.actual_events, name='view_events'),
+    path('e/', event_views.actual_events, name='show_events'),
     path('create/', event_views.create_event, name='create_event'),
     path('e/<event_id>/', event_views.event_detail, name='view_event'),
 ]
